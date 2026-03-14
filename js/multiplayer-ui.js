0fc5d744a4f7cdfd489a589591976a4203392af5
Brennan Hatton <brennan@brennanhatton.com>
2026-03-14 23:38:39 +1100

    fix: reset aiMode when entering online multiplayer lobby (#174)
    The Ready button was showing 'Start' in online multiplayer lobbies
    when a local/bot game had been played beforehand. This was because
    _aiMode was set to true on local game start but never reset when
    showLobby() was called for an online game.

--- js/multiplayer-ui.js
+++ js/multiplayer-ui.js
@@ -169,6 +169,7 @@ export class MultiplayerUI {
    */
   showLobby(payload) {
     this._lobbyState = { ...payload };
+    this._aiMode = false;
     this._myReady = false;
     // Initialize cam button from lobby settings (creator's chosen mode)
     if (this.inlineCamBtn) {
